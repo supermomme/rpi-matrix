da4122d54809ea6a702c9af6a8ae40eb93d75cd4
Momme Jürgensen <momme@juergensen.me>
2020-06-17 14:19:03 +0000

    some moar frontend

--- client/src/views/LayerList.vue
+++ client/src/views/LayerList.vue
@@ -4,40 +4,46 @@
     <v-btn @click="openCreate()" small outlined>
       Create new
     </v-btn>
-    <v-simple-table max-width="100px">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">Name</th>
-            <th class="text-left">Type</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(layer, index) in layers" :key="layer._id">
-            <td>{{ layer.name }}</td>
-            <td>{{ layer.type }}</td>
-            <td>
-              <v-btn @click="moveUp(layer, index)" icon small v-if="index !== 0">
-                <v-icon>mdi-arrow-up</v-icon>
-              </v-btn>
-            </td>
-            <td>
-              <v-btn @click="moveDown(layer, index)" icon small v-if="index !== layers.length-1">
-                <v-icon>mdi-arrow-down</v-icon>
-              </v-btn>
-            </td>
-            <td>
-              <v-btn @click="openSettings(layer)" small outlined>
-                Edit Settings
-              </v-btn>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <FeathersVuexFind service="layer" :query="{ $sort: { layer: -1 } }">
+      <section slot-scope="{ items: layers }">
+        <v-simple-table max-width="100px">
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Layer</th>
+                <th class="text-left">Name</th>
+                <th class="text-left">Type</th>
+                <th />
+                <th />
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(layer, index) in layers" :key="layer._id">
+                <td>{{ layer.layer }}</td>
+                <td>{{ layer.name }}</td>
+                <td>{{ layer.type }}</td>
+                <td>
+                  <v-btn @click="moveUp(layer._id)" icon small v-if="index !== 0">
+                    <v-icon>mdi-arrow-up</v-icon>
+                  </v-btn>
+                </td>
+                <td>
+                  <v-btn @click="moveDown(layer._id)" icon small v-if="index !== layers.length-1">
+                    <v-icon>mdi-arrow-down</v-icon>
+                  </v-btn>
+                </td>
+                <td>
+                  <v-btn @click="openSettings(layer)" small outlined>
+                    Edit Settings
+                  </v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </section>
+    </FeathersVuexFind>
     <v-dialog
       v-model="dialog"
       width="500"
@@ -67,32 +73,11 @@ export default {
     dialogLayerId: null
   }),
   computed: {
-    layers () {
-      return this.$store.getters['layer/list']
-    },
     dialogComponent () {
       return DialogComponents.find(component => component.name === this.dialogName) || 'div'
     }
-
-  },
-  watch: {
-    $route: {
-      handler: 'fetchData',
-      immediate: true
-    }
   },
   methods: {
-    async fetchData () {
-      this.loading = true
-      try {
-        console.log(await this.$store.dispatch('layer/find', { pagination: false }))
-      } catch (error) {
-        console.error(error)
-        this.error = error
-      } finally {
-        this.loading = false
-      }
-    },
     openSettings (layer) {
       this.dialogLayerId = null
       this.dialogName = 'clockSettings'
@@ -105,18 +90,39 @@ export default {
       this.dialogLayerId = 'new'
       this.dialog = true
     },
-    moveUp (layer, myIndex) {
-      console.log('moveUp')
-      const l1 = [...this.layers][myIndex]
-      const l2 = [...this.layers][myIndex - 1]
-      console.log(l1.layer, l2.layer)
-      this.$store.dispatch('layer/patch', [l1._id, { layer: l2.layer }])
-      console.log(l1.layer, l2.layer)
-      this.$store.dispatch('layer/patch', [l2._id, { layer: l1.layer }])
-      console.log(l1.layer, l2.layer)
+    async moveUp (layerId) {
+      const l1 = await this.$store.getters['layer/get'](layerId).clone()
+      const l2 = await this.$store.getters['layer/find']({
+        query: {
+          layer: {
+            $gt: l1.layer
+          },
+          $limit: 1,
+          $sort: { layer: 1 }
+        }
+      }).data[0].clone()
+      const oldl1Layer = l1.layer
+      l1.layer = l2.layer
+      l2.layer = oldl1Layer
+      l1.save()
+      l2.save()
     },
-    moveDown (layer, myIndex) {
-      console.log('moveDown')
+    async moveDown (layerId) {
+      const l1 = await this.$store.getters['layer/get'](layerId).clone()
+      const l2 = await this.$store.getters['layer/find']({
+        query: {
+          layer: {
+            $lt: l1.layer
+          },
+          $limit: 1,
+          $sort: { layer: -1 }
+        }
+      }).data[0].clone()
+      const oldl1Layer = l1.layer
+      l1.layer = l2.layer
+      l2.layer = oldl1Layer
+      l1.save()
+      l2.save()
     }
   }
 }
